714c7d4d859555a4fd86be2a9833b792e8626eb9
Kunal Bhat <kunal.bhat2001@gmail.com>
2022-10-30 20:53:18 +0530

    Setup state machine

--- graft/node.go
+++ graft/node.go
@@ -12,17 +12,20 @@ const (
 )
 
 type Node struct {
-	state     int
-	nodeId    int
-	term      int
-	leader    int
-	heartbeat bool
+	state      int
+	nodeId     int
+	currTerm   int
+	currLeader int
+	heartbeat  bool
 }
 
 func NodeConstructor(nodeId int) Node {
 	var node Node
 	node.state = Follower
 	node.nodeId = nodeId
+	node.currLeader = 0
+	node.currTerm = 0
+	node.heartbeat = false
 	return node
 }
 
@@ -30,20 +33,23 @@ func (node *Node) runStateMachine() {
 	lastSetTime := time.Now()
 	for {
 		if node.state == Follower {
-			if time.Since(lastSetTime) >= time.Second*3 && !node.heartbeat {
-				node.state = Candidate
-				fmt.Println(node.nodeId, " is a candidate")
-			} else {
-				node.heartbeat = !node.heartbeat
-				fmt.Println(node.nodeId, " has receieved a heartbeat from leader")
+			if time.Since(lastSetTime) >= time.Second*3 {
+				fmt.Println("inside")
+				if !node.heartbeat {
+					node.state = Candidate
+					fmt.Println(node.nodeId, " is a candidate")
+				} else {
+					fmt.Println(node.nodeId, " has receieved heartbeat from leader")
+					node.heartbeat = false
+					lastSetTime = time.Now()
+				}
 			}
-			lastSetTime = time.Now()
 		}
 		if node.state == Candidate {
-
+			fmt.Println(node.nodeId, " is requesting votes")
 		}
 		if node.state == Leader {
-
+			fmt.Println(node.nodeId, " is leader")
 		}
 	}
 }
