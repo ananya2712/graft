46adbb3126dc8de8035bc50160c516302b688209
Ananya Uppal <2712ananya@gmail.com>
2022-10-31 12:27:52 +0530

    heartbeat functions partial

--- graft/heartbeatHelper.go
+++ graft/heartbeatHelper.go
@@ -1,11 +1,50 @@
 package main
 
+import (
+	"log"
+	"net/rpc"
+	"strconv"
+)
+
 type heartBeatArgs struct {
 	id         int
 	currLeader int
 }
 
 type heartBeatReply struct {
-	success bool
-	id      int
+	success   bool
+	id        int
+	nextIndex int
+}
+
+func (node *Node) broadcastHeartBeat() {
+	var args = heartBeatArgs{
+		id:         node.nodeId,
+		currLeader: node.currLeader,
+	}
+	_ = args
+
+	for i := range node.peerList {
+		go func(i int) {
+			var reply heartBeatReply
+			node.sendHeartBeat(i, args, &reply)
+		}(i)
+	}
+}
+
+func (node *Node) sendHeartBeat(port int, args heartBeatArgs, reply *heartBeatReply) {
+	client, err := rpc.DialHTTP("tcp", "localhost:"+strconv.Itoa(port))
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	defer client.Close()
+	client.Call("Node.HeartBeat", args, reply)
+
+	if reply.success {
+		node.heartbeat = true
+	} else {
+		node.heartbeat = false
+		node.state = Follower
+	}
+	//incomplete - need to handle nextIndex
 }
